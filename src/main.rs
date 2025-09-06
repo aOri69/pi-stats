7167472307185f2470aaf0ec4f294cba22109c95
Aleksandr Sokolov <sashkaptits@gmail.com>
2025-09-07 00:27:33 +0100

    Default tick to 1 sec

--- src/main.rs
+++ src/main.rs
@@ -6,7 +6,7 @@ async fn main() -> Result<()> {
     color_eyre::install()?;
     let terminal = ratatui::init();
     let app_result = App::new()
-        .with_tick_duration(std::time::Duration::from_millis(500))
+        .with_tick_duration(std::time::Duration::from_millis(1000))
         .run(terminal)
         .await;
     ratatui::restore();
